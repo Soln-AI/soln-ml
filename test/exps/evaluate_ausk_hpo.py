0e537ee0fb31c129bc384c9a78ccbc39eee98121
shenyu <459240868@qq.com>
2020-03-01 20:39:17 +0800

    update

--- test/exps/evaluate_ausk_hpo.py
+++ test/exps/evaluate_ausk_hpo.py
@@ -64,6 +64,7 @@ def conduct_hpo(dataset='pc4', classifier_id='random_forest', iter_num=100, run_
 
 
 def conduct_ausk(dataset='pc4', classifier_id='random_forest', iter_num=100, run_id=0, seed=1, time_limit=3600):
+    from autosklearn.pipeline.components.classification.random_forest import RandomForest
     task_id = 'hpo-%s-%s-%d' % (dataset, classifier_id, iter_num)
     save_path = save_dir + '%ausk-s-%d.pkl' % (task_id, run_id)
 
@@ -71,7 +72,7 @@ def conduct_ausk(dataset='pc4', classifier_id='random_forest', iter_num=100, run
         time_left_for_this_task=int(time_limit),
         include_preprocessors=['no_preprocessing'],
         n_jobs=1,
-        include_estimators=['random_forest'],
+        include_estimators=[RandomForest],
         ensemble_memory_limit=8192,
         ml_memory_limit=8192,
         ensemble_size=1,
