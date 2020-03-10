640b221ccf7ec77e6f871bf58e6b1305c6678a90
shenyu <459240868@qq.com>
2020-03-10 19:06:51 +0800

    update poly

--- automlToolkit/components/feature_engineering/transformations/generator/polynomial_generator.py
+++ automlToolkit/components/feature_engineering/transformations/generator/polynomial_generator.py
@@ -1,9 +1,11 @@
 from ConfigSpace.configuration_space import ConfigurationSpace
-from ConfigSpace.hyperparameters import UnParametrizedHyperparameter
+from ConfigSpace.hyperparameters import UnParametrizedHyperparameter, UniformIntegerHyperparameter
 from automlToolkit.components.feature_engineering.transformations.base_transformer import *
 from automlToolkit.components.utils.configspace_utils import check_for_bool
 
 
+# TODO: Select top-k features for transformer
+# TODO: By what?
 class PolynomialTransformation(Transformer):
     def __init__(self, degree=2, interaction_only='True', include_bias='False', random_state=None):
         super().__init__("polynomial", 17)
@@ -12,21 +14,39 @@ class PolynomialTransformation(Transformer):
 
         self.output_type = NUMERICAL
         self.degree = degree
+        if self.degree == 2:
+            self.topn = 20
+        elif self.degree == 3:
+            self.topn = 10
+        else:
+            self.topn = 5
         self.interaction_only = interaction_only
         self.include_bias = include_bias
         self.random_state = random_state
+        self.select_model = None
 
     @ease_trans
     def operate(self, input_datanode, target_fields):
         from sklearn.preprocessing import PolynomialFeatures
+        from sklearn.feature_selection import SelectKBest, chi2
 
         X, y = input_datanode.data
         X_new = X[:, target_fields]
         ori_length = X_new.shape[1]
 
-        # Skip high-dimensional features.
-        if X_new.shape[1] > 100:
-            return X_new.copy()
+        # # Skip high-dimensional features.
+        # if X_new.shape[1] > 100:
+        #     return X_new.copy()
+        if not self.select_model:
+            if ori_length < self.topn:
+                k = 'all'
+            else:
+                k = self.topn
+            self.select_model = SelectKBest(score_func=chi2, k=k)
+            self.select_model.fit(X_new, y)
+
+        X_new = self.select_model.transform(X_new)
+        selected_length = X_new.shape[1]
 
         if not self.model:
             self.degree = int(self.degree)
@@ -39,16 +59,16 @@ class PolynomialTransformation(Transformer):
             self.model.fit(X_new)
 
         _X = self.model.transform(X_new)
-        if ori_length == 1:
+        if selected_length == 1:
             _X = _X[:, 1:]
         else:
-            _X = _X[:, ori_length + 1:]
+            _X = _X[:, selected_length + 1:]
 
         return _X
 
     @staticmethod
     def get_hyperparameter_search_space(dataset_properties=None):
-        degree = UnParametrizedHyperparameter(name="degree", value=2)
+        degree = UniformIntegerHyperparameter("degree", 2, 4, default_value=2)
         interaction_only = UnParametrizedHyperparameter("interaction_only", "True")
         include_bias = UnParametrizedHyperparameter("include_bias", "False")
 
